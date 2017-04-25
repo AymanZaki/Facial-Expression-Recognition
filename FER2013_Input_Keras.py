41c5cac6a08bf2742b3bf84c1de7894bda08df82
AymanZaki <ayman.zaki.az@gmail.com>
2017-04-25 16:58:30 +0200

    Resize Dataset Images

--- FER2013_Input_Keras.py
+++ FER2013_Input_Keras.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from PIL import Image
 from numpy import array
 from scipy.misc import toimage
+from scipy.misc import imresize
 from resizeimage import resizeimage
 
 
@@ -16,11 +17,19 @@ class FER2013_Input_Keras:
 	Testing_Images = []
 	Batch_Size = 128
 	path = ""
-	def __init__(self, path):
+	scale = 0
+	Scale_1_Length = 42
+	Scale_1_Width = 42
+	Scale_2_Length = 84
+	Scale_2_Width = 84
+	Scale_3_Length = 90
+	Scale_3_Width = 90
+	def __init__(self, path, scale):
 		self.path = path
-		[self.Training_labels, self.Training_Images] = self.FER2013_Training_Set()
+		self.scale = scale
+		"""[self.Training_labels, self.Training_Images] = self.FER2013_Training_Set()
 		[self.Validation_labels, self.Validation_Images] = self.FER2013_Validation_Set()
-		[self.Testing_labels, self.Testing_Images] = self.FER2013_Testing_Set()
+		[self.Testing_labels, self.Testing_Images] = self.FER2013_Testing_Set()"""
 		
 	def FER2013_Training_Set (self):
 			path = self.path + 'FER2013-Training.csv'
@@ -29,23 +38,10 @@ class FER2013_Input_Keras:
 			with open(path) as csvfile:
 				readCSV = csv.reader(csvfile, delimiter = ',')
 				#row[0] = Labels, row[1] = Image Pixels
-				for row in readCSV:
-					Label = int(row[0])
-					#Label = [0] * 7
-					#Label[int(row[0])] = 1
+				for row in readCSV:					
+					Label, ResizedImage = self.Get_Label_Image(row)
 					Training_labels.append(Label)
-					image = row[1].split()
-					#Cast strings to integer 
-					image = [int(i) for i in image]
-					#Convert 1D array to 2D array 48x48
-					Image48x48 = np.reshape(image, (48, 48))
-					#Convert 2D array to Image
-					Image42x42 = Image.fromarray(np.uint8(Image48x48))
-					#Resize Image to 42x42
-					Image42x42 = resizeimage.resize_contain(Image42x42, [42, 42])
-					#Convert Image to 2D array 
-					Image42x42 = np.uint8(Image42x42.convert('L'))
-					Training_Images.append(Image42x42)
+					Training_Images.append(ResizedImage)					
 			return  Training_labels, np.array(Training_Images)
 
 
@@ -57,22 +53,9 @@ class FER2013_Input_Keras:
 				readCSV = csv.reader(csvfile, delimiter = ',')
 				#row[0] = Labels, row[1] = Image Pixels
 				for row in readCSV:
-					Label = int(row[0])
-					#Label = [0] * 7
-					#Label[int(row[0])] = 1
+					Label, ResizedImage = self.Get_Label_Image(row)	
 					Validation_labels.append(Label)
-					image = row[1].split()
-					#Cast strings to integer 
-					image = [int(i) for i in image]
-					#Convert 1D array to 2D array 48x48
-					Image48x48 = np.reshape(image, (48, 48))
-					#Convert 2D array to Image
-					Image42x42 = Image.fromarray(np.uint8(Image48x48))
-					#Resize Image to 42x42
-					Image42x42 = resizeimage.resize_contain(Image42x42, [42, 42])
-					#Convert Image to 2D array 
-					Image42x42 = np.uint8(Image42x42.convert('L'))		
-					Validation_Images.append(Image42x42)
+					Validation_Images.append(ResizedImage)
 			return  Validation_labels, np.array(Validation_Images)
 
 	def FER2013_Testing_Set (self):
@@ -82,29 +65,32 @@ class FER2013_Input_Keras:
 			with open(path) as csvfile:
 				readCSV = csv.reader(csvfile, delimiter = ',')	
 				#row[0] = Labels, row[1] = Image Pixels
-				c = 0
 				for row in readCSV:
-					Label = int(row[0])
-					#Label = [0] * 7
-					#Label[int(row[0])] = 1
+					Label, ResizedImage = self.Get_Label_Image(row)
 					Testing_labels.append(Label)
-					image = row[1].split()
-					#Cast strings to integer 
-					image = [int(i) for i in image]
-					#Convert 1D array to 2D array 48x48  
-					Image48x48 = np.reshape(image, (48, 48))
-					#Convert 2D array to Image
-					Image42x42 = Image.fromarray(np.uint8(Image48x48))
-					#Resize Image to 42x42
-					Image42x42 = resizeimage.resize_contain(Image42x42, [42, 42])
-					#Convert Image to 2D array 
-					Image42x42 = np.uint8(Image42x42.convert('L'))
-					Testing_Images.append(Image42x42)
-					c = c + 1
-					#if c>=400:
-					#	break
+					Testing_Images.append(ResizedImage)
 			return  Testing_labels, np.array(Testing_Images)
 
+	def Get_Label_Image(self, row):
+			Label = int(row[0])
+			image = row[1].split()
+			#Cast strings to integer 
+			image = [int(i) for i in image]
+			#Convert 1D array to 2D array 48x48  
+			Image48x48 = np.reshape(image, (48, 48))
+			#Convert 2D array to Image
+			ResizedImage = Image.fromarray(np.uint8(Image48x48))
+			#Resize Image
+			if(self.scale == 1):
+				ResizedImage = imresize(ResizedImage, [self.Scale_1_Length, self.Scale_1_Width], interp='bilinear', mode=None)
+				#ResizedImage = resizeimage.resize_contain(ResizedImage, [self.Scale_1_Length, self.Scale_1_Width])
+			elif(self.scale == 2):
+				ResizedImage = imresize(ResizedImage, [self.Scale_2_Length, self.Scale_2_Width], interp='bilinear', mode=None)
+				#ResizedImage = resizeimage.resize_contain(ResizedImage, [self.Scale_2_Length, self.Scale_2_Width])
+			elif(self.scale == 3):
+				ResizedImage = imresize(ResizedImage, [self.Scale_3_Length, self.Scale_3_Width], interp='bilinear', mode=None)
+			return Label, ResizedImage
+"""
 	#Batch_Number 0 based
 	def Get_batch(self, Batch_Number, Mode):
 		begin = Batch_Number * self.Batch_Size
@@ -124,3 +110,4 @@ class FER2013_Input_Keras:
 			Labels = self.Testing_labels[begin:end]
 			Images = self.Testing_Images[begin:end]
 		return Labels, Images
+		"""
---
+++ FER2013_Model_Keras_Third_Scale.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+import keras
+import sys
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+from FER2013_Input_Keras import FER2013_Input_Keras
+import csv
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from numpy import array
+from scipy.misc import toimage
+from resizeimage import resizeimage
+
+batch_size = 128
+num_classes = 7
+epochs = int(sys.argv[1])
+
+img_rows, img_cols = 90, 90
+fer = FER2013_Input_Keras('/home/alaa/Desktop/GP/', 3)
+Training_labels, Training_Images = fer.FER2013_Training_Set()
+Testing_labels, Testing_Images = fer.FER2013_Testing_Set()
+Validation_labels, Validation_Images = fer.FER2013_Validation_Set()
+Training_Images = Training_Images.reshape(Training_Images.shape[0], img_rows, img_cols, 1)
+Validation_Images = Validation_Images.reshape(Validation_Images.shape[0], img_rows, img_cols, 1)
+Testing_Images = Testing_Images.reshape(Testing_Images.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+Training_Images = Training_Images.astype('float32')
+Validation_Images = Validation_Images.astype('float32')
+Testing_Images = Testing_Images.astype('float32')
+Training_Images /= 255
+Validation_Images/=255
+Testing_Images /= 255
+
+Training_labels = keras.utils.to_categorical(Training_labels, num_classes)
+Validation_labels = keras.utils.to_categorical(Validation_labels, num_classes)
+Testing_labels = keras.utils.to_categorical(Testing_labels, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(7, 7),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(3072, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=float(sys.argv[2]), decay=0.0, momentum=0.0, nesterov=False),
+              metrics=['accuracy'])
+
+model.fit(Training_Images, Training_labels,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(Validation_Images, Validation_labels))
+score = model.evaluate(Testing_Images, Testing_labels, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+'''
+model_json = model.to_json()
+with open('model'+sys.argv[3]+'.json', "w") as json_file:
+    json_file.write(model_json)
+
+model.save_weights('model'+sys.argv[3]+'_weights.h5')
+print('Model Saved!')
+
+
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# evaluate loaded model on test data
+loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+score = loaded_model.evaluate(Testing_Images, Testing_labels, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+'''
